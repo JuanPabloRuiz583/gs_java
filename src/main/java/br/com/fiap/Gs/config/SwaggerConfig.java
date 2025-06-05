388470965590a7fab53506dd18149f7abb3450ad
JuanPabloRuiz583 <juaruizdesouza@gmail.com>
2025-06-05 13:02:06 -0300

    configurando swagger config

--- src/main/java/br/com/fiap/Gs/config/SwaggerConfig.java
+++ src/main/java/br/com/fiap/Gs/config/SwaggerConfig.java
@@ -4,17 +4,20 @@ import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${spring.profiles.active:local}")
+    private String activeProfile;
+
     @Bean
     public OpenAPI customOpenAPI() {
         final String securitySchemeName = "bearerAuth";
-        return new OpenAPI()
-                .addServersItem(new Server().url("https://gsjava-production-c327.up.railway.app"))
+        OpenAPI openAPI = new OpenAPI()
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new io.swagger.v3.oas.models.Components()
                         .addSecuritySchemes(securitySchemeName,
@@ -23,5 +26,12 @@ public class SwaggerConfig {
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
                                         .bearerFormat("JWT")));
+
+        if ("local".equals(activeProfile)) {
+            openAPI.addServersItem(new Server().url("http://localhost:8080"));
+        } else {
+            openAPI.addServersItem(new Server().url("https://gsjava-production-c327.up.railway.app"));
+        }
+        return openAPI;
     }
 }
