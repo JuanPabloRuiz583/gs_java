28789505856c45034d62957411aec73613cb11bf
JuanPabloRuiz583 <juaruizdesouza@gmail.com>
2025-06-05 14:09:01 -0300

    cors config

---
+++ src/main/java/br/com/fiap/Gs/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package br.com.fiap.Gs.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8080", "https://gsjava-production-c327.up.railway.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
